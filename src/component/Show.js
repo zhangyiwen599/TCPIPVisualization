d71898bef599571e72e66f5433ad60192b64f61e
SenW <1144165853@qq.com>
2019-11-20 14:52:14 +0800

    correct warnings

--- src/component/Show.js
+++ src/component/Show.js
@@ -5,104 +5,98 @@ import SubBlock from './SubBlock';
 const num = 124;
 
 class Show extends Component {
-    constructor(props){
-        super(props)
-        
-    }
-
- 
     render() {
         return (
             <div>
-            
+
                 <Block idName="senderAppl">
-                    <SubBlock divStyle={{width:"10%"}}>                 
-                        {this.props.data.context}{this.props.data.isReady[0]}                    
-                    </SubBlock>   
+                    <SubBlock divStyle={{ width: "10%" }}>
+                        {this.props.data.context}{this.props.data.isReady[0]}
+                    </SubBlock>
                 </Block>
-               
+
                 <Block idName="senderTran">
-                    <SubBlock divStyle={{width:"10%"}}>
+                    <SubBlock divStyle={{ width: "10%" }}>
                         {this.props.data.context}{this.props.data.isReady[1]}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
                 </Block>
 
-                
+
                 <Block idName="senderNet">
-                    <SubBlock divStyle={{width:"10%"}}>
+                    <SubBlock divStyle={{ width: "10%" }}>
                         {this.props.data.context}{this.props.data.isReady[2]}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
                 </Block>
-                
+
 
                 <Block idName="senderDataChain">
-                    <SubBlock divStyle={{width:"10%"}}>
-                    {this.props.data.context}{this.props.data.isReady[3]}
+                    <SubBlock divStyle={{ width: "10%" }}>
+                        {this.props.data.context}{this.props.data.isReady[3]}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
                 </Block>
-                
+
                 {/******************************* */}
                 {/***********    reciver     ********** */}
                 {/******************************* */}
 
                 <Block idName="reciverAppl">
-                    <SubBlock divStyle={{width:"10%"}}>
-                    {num}    
-                    </SubBlock>   
-                </Block>
-               
-                <Block idName="reciverTran">
-                    <SubBlock divStyle={{width:"10%"}}>
-                        {num}
-                    </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "10%" }}>
                         {num}
                     </SubBlock>
                 </Block>
 
-                
-                <Block idName="reciverNet">
-                    <SubBlock divStyle={{width:"10%"}}>
+                <Block idName="reciverTran">
+                    <SubBlock divStyle={{ width: "10%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
-                        {num}
-                    </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
                 </Block>
-                
+
+
+                <Block idName="reciverNet">
+                    <SubBlock divStyle={{ width: "10%" }}>
+                        {num}
+                    </SubBlock>
+                    <SubBlock divStyle={{ width: "20%" }}>
+                        {num}
+                    </SubBlock>
+                    <SubBlock divStyle={{ width: "20%" }}>
+                        {num}
+                    </SubBlock>
+                </Block>
+
 
                 <Block idName="reciverDataChain">
-                    <SubBlock divStyle={{width:"10%"}}>
+                    <SubBlock divStyle={{ width: "10%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
-                    <SubBlock divStyle={{width:"20%"}}>
+                    <SubBlock divStyle={{ width: "20%" }}>
                         {num}
                     </SubBlock>
                 </Block>
--- src/component/SubBlock.js
+++ src/component/SubBlock.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react'
 
 class SubBlock extends Component {
 
-    
+
 
     render() {
         return (
             <div id={this.props.idName} className="subblock" style={this.props.divStyle}>
-                {React.Children.toArray(this.props.children)[1]==1 && this.props.children[0]}
+                {React.Children.toArray(this.props.children)[1] === 1 && this.props.children[0]}
             </div>
         )
     }
